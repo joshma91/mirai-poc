d7dcd31d8abc31d2127482d181c5a5341b81321e
Joshma91 <josh.ma91@gmail.com>
2018-07-12 18:52:03 -0400

    re-added the Product functions and tests dropped from feature-product branch

--- dapp/test/Products.js
+++ dapp/test/Products.js
@@ -42,13 +42,21 @@ contract('Testing Products contract', function(accounts) {
       thirdProduct.available,
       { from: accounts[0] })
 
-    const productIds = await products.getProductIdsByOwner.call( accounts[0], { from: accounts[0] })
-    const productIdsNum = productIds.map(x => x.toNumber()) 
+    const productIdsAcct0 = await products.getProductIdsByOwner.call( accounts[0], { from: accounts[0] })
+    const productIdsNumAcct0 = productIdsAcct0.map(x => x.toNumber()) 
     
+    const allProductIds = await products.getAllProductIds.call ({ from: accounts[0] })
+    const allProductIdsNum = allProductIds.map(x => x.toNumber())
+
     const price = await products.getPriceById.call(3, { from: accounts[0] })
     
-    expect(productIdsNum.toString()).to.equal("1,2")
+    const prod1 = await products.getProductById.call(2, { from: accounts[0] })
+    const prod1Owner = prod1[1];
+
+    expect(productIdsNumAcct0.toString()).to.equal("1,2")
+    expect(allProductIdsNum.toString()).to.equal("1,2,3")
     expect(price.toNumber()).to.equal(2)
+    expect(prod1Owner).to.equal(accounts[0])
 
   })
 })
