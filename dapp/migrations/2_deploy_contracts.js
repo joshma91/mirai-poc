442aaf38d4d057441d0a08e95134ca1ee8cb100c
Joshma91 <josh.ma91@gmail.com>
2018-07-23 16:35:21 -0400

    replaced DSToken with generic ERC20, clean up names

--- dapp/migrations/2_deploy_contracts.js
+++ dapp/migrations/2_deploy_contracts.js
@@ -1,21 +1,21 @@
 const TokenRegistry = artifacts.require("./tokens/TokenRegistry.sol");
-const DSToken = artifacts.require("./tokens/DSToken.sol");
-const POPToken = artifacts.require("./tokens/POPToken.sol");
-const Products = artifacts.require("./tokens/Products");
+const TestToken = artifacts.require("./tokens/TestToken.sol");
+const MiraiOwnership = artifacts.require("./tokens/MiraiOwnership.sol");
+const MiraiCore = artifacts.require("./tokens/MiraiCore");
 
 module.exports = function(deployer) {
   deployer.deploy(TokenRegistry).then(async registry => {
-    const daiToken = await deployer.deploy(DSToken, "DAI");
-    await deployer.deploy(POPToken, "POPToken", "POP", registry.address);
-    await deployer.deploy(Products);
-    await setRegistry(daiToken, registry);
+    const testToken = await deployer.deploy(TestToken);
+    await deployer.deploy(MiraiOwnership, "MiraiOwnership", "POP", registry.address);
+    await deployer.deploy(MiraiCore);
+    await setRegistry(testToken, registry);
   });
 };
 
 async function setRegistry(contract, registry) {
-  const contractName = web3.toAscii(await contract.name());
-  const contractSymbol = web3.toAscii(await contract.symbol.call());
-  const contractDecimals = await contract.decimals();
+  const contractName = await contract.name.call();
+  const contractSymbol = await contract.symbol.call();
+  const contractDecimals = await contract.decimals.call();
 
   await registry.addToken(
     contract.address,
