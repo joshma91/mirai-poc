3b6016fafc36240e6a5b525639db36d6dc3e8c7f
Adrian Li <li.adrianmc@gmail.com>
2018-07-26 00:00:43 -0400

    refactor and add start script

--- server/app.test.js
+++ server/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("./index");
+const app = require("./app");
 
 describe("Test the root path", () => {
   test("It should response the GET method", async () => {
