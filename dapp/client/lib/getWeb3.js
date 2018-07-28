8b01a073628b4f2e606d1b7bfec2797fb4a1aff7
Joshma91 <josh.ma91@gmail.com>
2018-07-27 22:02:16 -0700

    update web3 implementation to best practices. retrieve bookId on front-end

--- dapp/client/lib/getWeb3.js
+++ dapp/client/lib/getWeb3.js
@@ -1,31 +1,30 @@
-/* globals window, document */
-import Web3 from "web3";
+import Web3 from 'web3'
 
 const resolveWeb3 = (resolve) => {
-  let { web3 } = window;
-  const alreadyInjected = typeof web3 !== `undefined`; // i.e. Mist/Metamask
+  let { web3 } = window
+  const alreadyInjected = typeof web3 !== 'undefined' // i.e. Mist/Metamask
+  const localProvider = `http://localhost:9545`
 
   if (alreadyInjected) {
-    web3 = new Web3(web3.currentProvider);
-    console.log(`Injected web3 detected.`);
-    resolve(web3);
+    console.log(`Injected web3 detected.`)
+    web3 = new Web3(web3.currentProvider)
   } else {
-    // Fallback to localhost if no web3 injection.
-    const provider = new Web3.providers.HttpProvider(`http://localhost:9545`);
-    web3 = new Web3(provider);
-    console.log(`No web3 instance injected, using Local web3.`);
-    resolve(web3);
+    console.log(`No web3 instance injected, using Local web3.`)
+    const provider = new Web3.providers.HttpProvider(localProvider)
+    web3 = new Web3(provider)
   }
-};
+
+  resolve(web3)
+}
 
 export default () =>
   new Promise((resolve) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener(`load`, () => {
-      resolveWeb3(resolve);
-    });
+      resolveWeb3(resolve)
+    })
     // If document has loaded already, try to get Web3 immediately.
     if (document.readyState === `complete`) {
-      resolveWeb3(resolve);
+      resolveWeb3(resolve)
     }
-  });
+  })
