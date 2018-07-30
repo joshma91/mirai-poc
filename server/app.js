ccc8e5c390871901feb47f4772f0f125be17fcf7
Adrian Li <li.adrianmc@gmail.com>
2018-07-30 02:43:32 -0400

    use morgan for logging

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 const books = {};
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.get("/books", (req, res) => {
   const { bookId } = req.query;
-  console.log('GET request logged')
-  console.log('bookId', bookId)
   if (books[bookId]) {
     return res.status(200).json({ bookTitle: books[bookId] });
   }
@@ -20,8 +20,6 @@ app.get("/books", (req, res) => {
 
 app.post("/books", (req, res) => {
   const { bookId, bookTitle } = req.body;
-  console.log('POST request logged')
-  console.log('body', req.body)
   books[bookId] = bookTitle;
   return res.sendStatus(200);
 });
