a10f7baa15910cd7d73aad4ba62ee46998753608
Adrian Li <li.adrianmc@gmail.com>
2018-07-30 02:38:17 -0400

    add cors management library

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 const books = {};
 
+app.use(cors());
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.get("/books", (req, res) => {
   const { bookId } = req.query;
+  console.log('GET request logged')
+  console.log('bookId', bookId)
   if (books[bookId]) {
-    res.status(200).json({ bookTitle: books[bookId] });
+    return res.status(200).json({ bookTitle: books[bookId] });
   }
-  res.sendStatus(404);
+  return res.sendStatus(404);
 });
 
 app.post("/books", (req, res) => {
   const { bookId, bookTitle } = req.body;
+  console.log('POST request logged')
+  console.log('body', req.body)
   books[bookId] = bookTitle;
-  res.sendStatus(200);
+  return res.sendStatus(200);
 });
 
 module.exports = app;
