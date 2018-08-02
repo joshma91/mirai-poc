594ecf1804d7a26fa8a15073fa9183bcc5516467
Adrian Li <li.adrianmc@gmail.com>
2018-08-02 01:07:37 -0400

    clean up styles

--- dapp/client/components/ProductItem.js
+++ dapp/client/components/ProductItem.js
@@ -1,4 +1,12 @@
-import { Image, Grid, Header, Button, List, Icon } from "semantic-ui-react";
+import {
+  Image,
+  Grid,
+  Header,
+  Button,
+  List,
+  Icon,
+  Loader
+} from "semantic-ui-react";
 const API_URL = "http://localhost:5678/books";
 
 export default class ProductItem extends React.Component {
@@ -32,23 +40,23 @@ export default class ProductItem extends React.Component {
 
   render() {
     const { product } = this.state;
-    return product ? (
-      <Grid columns={2} verticalAlign="bottom">
-        <Grid.Row>
-          <Grid.Column>
-            <Image src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" />
-          </Grid.Column>
-          <Grid.Column>
-            <Grid.Row> Title: {product.title} </Grid.Row>
-            <Grid.Row> Price: {product.price}</Grid.Row>
-            <Grid.Row>
-              <Button>Buy!</Button>
-            </Grid.Row>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    ) : (
-      <div />
+    if (!product) return <Loader active />;
+    return (
+      <div>
+        <img
+          className="product-image"
+          src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png"
+          alt=""
+        />
+        <div className="title">{product.title}</div>
+        <div className="price">{product.price}</div>
+        <Button>Buy!</Button>
+        <style jsx>{`
+          .product-image {
+            width: 100%;
+          }
+        `}</style>
+      </div>
     );
   }
 }
