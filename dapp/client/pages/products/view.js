7b1970c61676fdc1b499088944fb5116a62d0673
Joshma91 <josh.ma91@gmail.com>
2018-08-09 16:23:01 -0700

    new page for viewing purchased items. reads tokenURI to find productId

---
+++ dapp/client/pages/products/view.js
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import { Header, Button } from "semantic-ui-react";
+
+import Layout from "../../components/Layout";
+import Web3Container from "../../lib/Web3Container";
+import MiraiOwnershipJSON from "../../lib/contracts/MiraiOwnership.json";
+
+class ViewProducts extends React.Component {
+  state = { productIds: null };
+
+  componentDidMount = async () => {
+    const { accounts, contract } = this.props;
+
+    const tokenIds = await contract.methods
+      .getTokensByOwner(accounts[0])
+      .call({ from: accounts[0], gas: 3000000 });
+
+    const productIds = await this.getProductIdsFromTokens(tokenIds);
+
+    this.setState({ productIds });
+  };
+
+  getProductIdsFromTokens = async tokenIds => {
+    const { accounts, contract } = this.props;
+    const idPromises = tokenIds.map(id => {
+      return contract.methods.tokenURI(id).call({ from: accounts[0], gas: 3000000 });
+    });
+    const productIds = await Promise.all(idPromises);
+
+    // filter out duplicates. Can be enforced on the blockchain side but takes more work
+    return [...new Set(productIds)];
+  };
+
+  renderProducts() {
+    const { productIds } = this.state;
+
+    if (productIds.length === 0) return <div>No Items Found</div>;
+    return (
+      <div className="wrapper">{productIds.map(id => <div>{id}</div>)}</div>
+    );
+  }
+
+  render() {
+    const { productIds } = this.state;
+    return (
+      <Layout>
+        <Link href="/">
+          <Button>Home</Button>
+        </Link>
+
+        <hr />
+
+        <Header as="h1">My Products</Header>
+        {productIds ? this.renderProducts() : "Loading..."}
+      </Layout>
+    );
+  }
+}
+
+export default () => (
+  <Web3Container
+    contractJSON={MiraiOwnershipJSON}
+    renderLoading={() => <div>Loading Page...</div>}
+    render={({ web3, accounts, contract }) => (
+      <ViewProducts accounts={accounts} contract={contract} web3={web3} />
+    )}
+  />
+);
